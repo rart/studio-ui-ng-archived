0a7f80aa10a6b89b94a7088faad133ce14ddc0c0
Roy Art <royart@me.com>
2016-04-12 14:51:36 +0200

    Upppercase product name

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -3,5 +3,5 @@ import {Component} from 'angular2/core';
 @Component({
     templateUrl: 'components/home/home.html'
 }) export class HomeCmp {
-    public message: String = 'Welcome to studio 3.';
+    public message: String = 'Welcome to Studio 3.';
 }
